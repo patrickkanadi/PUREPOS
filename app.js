3dd5548a769912b80c88a962bbf00da26bbbdfc9
patrickkanadi <patrick.kanadi@gmail.com>
2026-09-04 20:55:50 +0700

    Update app.js

--- app.js
+++ app.js
@@ -76,7 +76,8 @@ window.hashPIN = async function(pin) {
 window.getWibDate = function() {
     const d = new Date(); const utc = d.getTime() + (d.getTimezoneOffset() * 60000); const nd = new Date(utc + (3600000 * 7)); 
     const pad = n => n < 10 ? '0' + n : n;
-    return `${nd.getFullYear()}-${pad(nd.getMonth()+1)}-${pad(nd.getDate())} ${pad(nd.getHours())}:${pad(nd.getMinutes())}:${pad(nd.getSeconds())}`;
+    // USA Format: MM/DD/YYYY
+    return `${pad(nd.getMonth()+1)}/${pad(nd.getDate())}/${nd.getFullYear()} ${pad(nd.getHours())}:${pad(nd.getMinutes())}:${pad(nd.getSeconds())}`;
 }
 
 window.addEventListener('beforeinstallprompt', (e) => {
